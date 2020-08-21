9c5c8b9ba4e9c56f41b362cae1a9cb62c1721dbf
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-08-21 21:22:37 +0200

    add basic implementation to cropper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import Cropper from "cropperjs";
+import testImage from "./1584.jpg";
+import "./App.css";
+import "/Users/juanramirezcastaneda/DevelopmentProjects/cropper-test/node_modules/cropperjs/dist/cropper.css"
 
 function App() {
+  let srcImage = useRef(null);
+
+  function activateCropper() {
+    const cropper = new Cropper(srcImage.current);
+    console.log(cropper);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <div style={{ width: "821px", height: "497px" }}>
+          <div className={"cropper-wrap-box"}>
+            <div
+              className={"cropper-canvas"}
+              style={{
+                width: "993.41px",
+                height: "558.793px",
+                transform: "translateX(-91.07px) translateY(-91.07px)",
+              }}
+            >
+              <img
+                src={testImage}
+                ref={srcImage}
+                style={{ width: '993.41px', height: '558.793px', transform: 'none'}}
+                alt="highresolution"
+              />
+            </div>
+          </div>
+        </div>
+
+        <input
+          type="button"
+          value="Activate Cropper"
+          onClick={activateCropper}
+        />
+      </section>
     </div>
   );
 }
